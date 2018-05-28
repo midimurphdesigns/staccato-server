f61367038229ba8169f1ebe89ed336a899bf68f0
midimurphdesigns <midimurphdesigns@gmail.com>
2018-05-28 13:47:36 -0700

    post endpoint

--- routes/question.js
+++ routes/question.js
@@ -8,7 +8,25 @@ router.get('/questions', (req, res, err) => {
   Question.find()
     .then(result => {
       res.json(result);
-    });
+    })
+    .catch(err => console.log(err));
 });
+
+router.post('/questions', (req, res, next) => {
+  const { question, answer } = req.body;
+  const newQuestion = { question, answer };
+
+  Question.create(newQuestion)
+    .then(result => {
+      res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
+    })
+    .catch(err => {
+      if (err) {
+        err = new Error('There\'s an error')
+        err.status = 400;
+      }
+      next(err);
+    });
+  });
 
 module.exports = router;
