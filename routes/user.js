594baee2234598c7b54a461a85aa57081e328edb
midimurphdesigns <midimurphdesigns@gmail.com>
2018-05-30 12:12:21 -0700

    fixed next endpoint

--- routes/user.js
+++ routes/user.js
@@ -15,12 +15,14 @@ router.get('/', (req, res, err) => {
     .catch(err => console.log(err));
 });
 
-router.get('/:id', (req, res, next) => {
-  const { id } = req.params;
-  User.findOne({ _id: id })
+const jwtAuth = passport.authenticate('jwt', {session:false});
+
+router.get('/next', jwtAuth, (req, res, next) => {
+  console.log('USER',req.user);
+  User.findByIdAndUpdate(req.user.id, {$inc:{ head: 1 }}).exec()
     .then(result => {
       if (result) {
-        res.status(200).json(result);
+        res.status(200).json(result.questions[result.head]);
       }
       else {
         next();
@@ -31,14 +33,12 @@ router.get('/:id', (req, res, next) => {
     });
 });
 
-const jwtAuth = passport.authenticate('jwt', {session:false});
-
-router.get('/next/:id', (req, res, next) => {
-  console.log('USER',req.user);
-  User.findByIdAndUpdate({_id:req.params.id}, {$inc:{head:1}}).exec()
+router.get('/:id', (req, res, next) => {
+  const { id } = req.params;
+  User.findOne({ _id: id })
     .then(result => {
       if (result) {
-        res.status(200).json(result.questions[result.head]);
+        res.status(200).json(result);
       }
       else {
         next();
