dd4f0f63974a108df0b13335a831706f46410dfc
JacobAZimmerman <ijacobzim@gmail.com>
2018-05-30 19:33:58 -0700

    questions loop now

--- routes/user.js
+++ routes/user.js
@@ -17,10 +17,24 @@ router.get('/', (req, res, err) => {
 
 const jwtAuth = passport.authenticate('jwt', {session:false});
 
-router.get('/next', jwtAuth, (req, res, next) => {
-  console.log('USER',req.user);
-  User.findByIdAndUpdate(req.user.id, {$inc:{ head: 1 }}, { new: true }).exec()
+router.post('/next', jwtAuth, (req, res, next) => {
+  console.log(req.body);
+  //console.log('USER',req.user);
+
+  // point the questions.next to head, set this last on to cycle from back to front
+  // User.updateOne({_id: req.user.id}, {"questions.next" : null}, {$set: { "questions.$.next": {$inc: { head: 1 }}}}).exec()
+  console.log('Qs',req.user.questions.length, 'head', req.user.head);
+  User.findById(req.user.id)
     .then(result => {
+      if (result.head >= (result.questions.length-1)) {
+        return User.findByIdAndUpdate(req.user.id, {$set:{ head: 0 }}, { new: true }).exec();
+      }
+      else {
+        return User.findByIdAndUpdate(req.user.id, {$inc:{ head: 1 }}, { new: true }).exec();
+      }
+    })
+    .then(result => {
+      console.log('RESULT', result);
       if (result) {
         res.status(200).json(result.questions[result.head]);
       }
@@ -31,6 +45,38 @@ router.get('/next', jwtAuth, (req, res, next) => {
     .catch(err => {
       next(err);
     });
+  // if (req.user.head >= (req.user.questions.length-1)) {
+  //   console.log('got here');
+  //   User.findByIdAndUpdate(req.user.id, {$set:{ head: 0 }}, { new: true }).exec()
+  //     .then(result => {
+  //       console.log('RESULT', result);
+  //       if (result) {
+  //         res.status(200).json(result.questions[result.head]);
+  //       }
+  //       else {
+  //         next();
+  //       }
+  //     })
+  //     .catch(err => {
+  //       next(err);
+  //     });
+  // }
+  // else {
+  //   console.log('ELSE');
+  //   User.findByIdAndUpdate(req.user.id, {$inc:{ head: 1 }}, { new: true }).exec()
+  //     .then(result => {
+  //       console.log('RESULT', result);
+  //       if (result) {
+  //         res.status(200).json(result.questions[result.head]);
+  //       }
+  //       else {
+  //         next();
+  //       }
+  //     })
+  //     .catch(err => {
+  //       next(err);
+  //     });
+  // }
 });
 
 router.get('/:id', (req, res, next) => {
@@ -47,6 +93,8 @@ router.get('/:id', (req, res, next) => {
     .catch(err => {
       next(err);
     });
+  // double the increment if userInput is true
+  // set to 1 if false
 });
 
 module.exports = router;
